c143ba35127e8e7cec34f91cc112654ad015b92c
tzeRueiWong <151814241+tzeRueiWong@users.noreply.github.com>
2023-12-04 15:52:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os 
-
+from apikey import apikey
 
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 
 
 
-st.title('some random title')
+os.environ['OPENAI_API_KEY'] = apikey 
+
+st.title('sample youtube title')
 prompt = st.text_input('Plug in your prompt')
+
+with st.sidebar:
+    selected  = option_menu(
+        menu_title = "Main Menu",
+        options = ["Home","Projects","Contact"]
+    )
+
 
